3d4800323c4200040c6eb8ea77f695dda4809372
Emanuel-developer992 <Orion_Developer@outlook.com>
2023-03-06 14:46:27 -0300

    06/03/2023

--- App.js
+++ App.js
@@ -1,28 +1,47 @@
 import { Canvas, useFrame } from '@react-three/fiber';
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
+
 
 export default function App() {
+  const [active, setActive] = useState(false);
+  const [locationX, setLocationX] = useState(0);
+  const [locationY, setLocationY] = useState(0);
+  const [position, setPosition] = useState(0);
+  const [click, setClick] = useState(false);
+  const [clickX, setClickX] = useState(0);
+  const [clickY, setClickY] = useState(0);
+
+  const myMesh = useRef();
 
   function Box(props) {
-    const [active, setActive] = useState(false);
 
+    useFrame(() => {
+      //const a = clock.getElapsedTime()
+      //myMesh.current.rotation.y = 
+
+
+      myMesh.current.rotation.y = locationX/20;
+      myMesh.current.rotation.x = locationY/20;
+   
+      
+    })
+    
     return (
       <mesh  {...props}
       onClick={(event) => setActive(!active)}
-      scale={active ? 1.5 : 1}
+      ref={myMesh}
+      onPointerMove={(e) => {setLocationX(e.locationX); setLocationY(e.locationY)}}
+      rotation={[0, 0, 0]}
       >
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={'#ff0040'} />
+        <boxGeometry args={[3, 1, 1]} />
+        <meshStandardMaterial color={'#4b176d'} />
         <ambientLight />
-        <pointLight position={[12, 12, 12]} />
+        <pointLight position={[10, 10, 10]} />
       </mesh>
     );
   }
 
   return <Canvas>
-        <Box position={[1, 1.2, 0]} />
-        <Box position={[-1, 1.2, 0]} />
-        <Box position={[1, -1.2, 0]} />
-        <Box position={[-1, -1.2, 0]} />
+        <Box position={[0, 0, 0]} />
   </Canvas>;
 }
